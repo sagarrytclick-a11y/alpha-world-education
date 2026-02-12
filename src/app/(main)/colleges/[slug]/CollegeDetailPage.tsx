190d631ee67aa45f99b1042c8476c0b12ce88e19
apple <apple@apples-MacBook-Pro.local>
2026-02-12 14:53:07 +0530

    search-mobile

--- src/app/(main)/colleges/[slug]/CollegeDetailPage.tsx
+++ src/app/(main)/colleges/[slug]/CollegeDetailPage.tsx
@@ -287,7 +287,7 @@ const CollegeDetailPage: React.FC<CollegeDetailPageProps> = ({ slug }) => {
                         {/* <span className="absolute -bottom-1 left-0 w-0 h-[2px] bg-blue-500 group-hover:w-full transition-all duration-300" /> */}
                       </span>
 
-                      <div className={`w-6 h-6 rounded-full bg-blue-50 flex items-center justify-center group-hover:text-white transition-all duration-300 ${isExpanded ? 'rotate-180' : 'rotate-0'}`}>
+                      <div className={`w-6 h-6 rounded-full bg-blue-50 flex items-center justify-center  transition-all duration-300 ${isExpanded ? 'rotate-180' : 'rotate-0'}`}>
                         {isExpanded ? (
                           <ChevronUp className="w-3 h-3" />
                         ) : (
